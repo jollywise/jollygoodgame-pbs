8e24f26323f592dff6f9d1e7a39d118cff1f9e4d
puzzledmonkey <chris@oedo.net>
2020-10-09 23:18:34 +0900

    add listeners for springroll

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,29 @@
 import { StoragePlugin, TrackingPlugin, SettingsPlugin } from './pbs/plugins';
+import { SafeScaleManager } from 'springroll';
 
 export const addPlugins = (scope, opts, storage) => {
   const { springRoll } = opts;
   scope.springRoll = springRoll;
+
+  springRoll.state.pause.subscribe((value) => {
+    console.log('SpringRoll paused', value);
+  });
+
+  springRoll.state.soundVolume.subscribe((value) => {
+    console.log('SpringRoll volume', value);
+  });
+
+  scope.events.once('boot', () => {
+    scope.safeScale = new SafeScaleManager({
+      width: scope.defaultWidth,
+      height: scope.defaultHeight,
+      safeWidth: scope.safeWidth || scope.defaultWidth,
+      safeHeight: scope.safeHeight || scope.defaultHeight,
+      callback: (opts) => {
+        console.log('Window scale changed', opts);
+      },
+    });
+  });
 
   // PBS Saves
   storage.plugin = new StoragePlugin(springRoll);
