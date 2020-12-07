8594993e065d88cf853bf6882e8137cc6dd70fe7
Stephen Hamilton <stephenhamilton@mac.com>
2020-12-07 23:24:41 +0000

    Modifies helpers for springroll scaling
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

--- src/helpers.js
+++ src/helpers.js
@@ -6,6 +6,7 @@ export const addPlugins = (scope, opts, storage) => {
   const { defaultDimensions, safeDimensions } = scope;
   scope.springRoll = springRoll;
 
+  // PBS Pause subscription
   springRoll.state.pause.subscribe((value) => {
     console.log('SpringRoll paused', value);
     if (value) {
@@ -15,17 +16,34 @@ export const addPlugins = (scope, opts, storage) => {
     }
   });
 
-  scope.events.once('boot', () => {
-    scope.safeScale = new SafeScaleManager({
-      width: defaultDimensions.width,
-      height: defaultDimensions.height,
-      safeWidth: safeDimensions.width || defaultDimensions.width,
-      safeHeight: safeDimensions.height || defaultDimensions.height,
-      callback: (opts) => {
-        console.log('Window scale changed', opts);
-      },
+  // PBS Scaling
+  const onWindowResize = (opts) => {
+    // https://github.com/SpringRoll/Springroll-Seed/blob/templates/phaser3/src/SpringrollGame.js
+    if (!scope.canvas) {
+      return;
+    }
+    const { scaleRatio } = opts;
+    scope.canvas.style.width = `${defaultDimensions.width * scaleRatio}px`;
+    scope.canvas.style.height = `${defaultDimensions.height * scaleRatio}px`;
+    // pass on the springroll scale values to viewportController
+    scope.viewportController.updateViewportCanvasBounds(opts);
+  };
+
+  const options = {
+    width: defaultDimensions.width,
+    height: defaultDimensions.height,
+    safeWidth: safeDimensions.width || defaultDimensions.width,
+    safeHeight: safeDimensions.height || defaultDimensions.height,
+    callback: onWindowResize,
+  };
+
+  if (scope.isBooted) {
+    scope.safeScale = new SafeScaleManager(options);
+  } else {
+    scope.events.once('boot', () => {
+      scope.safeScale = new SafeScaleManager(options);
     });
-  });
+  }
 
   // PBS Saves
   storage.plugin = new StoragePlugin(springRoll);
