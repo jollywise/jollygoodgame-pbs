34b761a352c12b01c587e11d4506cf0713d0faab
puzzledmonkey <chris@oedo.net>
2021-03-15 20:27:22 +0900

    fix build paths

--- buildtools/webpack_config/constants/dist/paths.js
+++ buildtools/webpack_config/constants/dist/paths.js
@@ -1,22 +1,20 @@
 const path = require('path');
 const CONTENT_BASE = 'src';
-const TEMPLATES = 'buildtools/templates/local';
-const projectRoot = path.resolve(__dirname, '../../../../'); // used in jollyapps to target correct folders
-const root = path.resolve(__dirname, '../../../../');
-const dist = path.join(projectRoot, 'dist/');
-const htmlOutPath = path.join(projectRoot, 'dist/');
+const TEMPLATES = path.resolve(__dirname, '../../../templates/local');
+const root = path.resolve('.');
+const dist = path.join(root, 'dist/');
+const htmlOutPath = path.join(root, 'dist/');
 const src = path.join(root, CONTENT_BASE);
 const libsDir = path.join(src, 'libs');
 const nodeDir = path.join(root, 'node_modules');
 
 const PATHS = {
-  projectRoot,
   root,
   dist,
   nodeDir,
   src,
   libsDir,
-  templateDir: path.join(root, TEMPLATES),
+  templateDir: TEMPLATES,
   fonts: 'css/fonts/[name].[ext]',
   images: 'images/[name].[ext]',
   entryFile: src + '/js/main.js',
