3e8d12c36401e926508242b1abc5bac9b5fe4928
puzzledmonkey <chris@oedo.net>
2021-03-24 15:54:13 +0900

    fix env and paths

--- buildtools/skeleton/src/js/main.js
+++ buildtools/skeleton/src/js/main.js
@@ -1,16 +1,9 @@
-/* __SHORTCUTS_ENABLED__:false */
-
 import './../scss/main.scss';
 import App from 'App';
 import Boot from 'game/scenes/Boot';
 import Load from 'game/scenes/Load';
 import { dOMReady, getConfigBase } from '@jollywise/jollygoodgame';
 
-let SHORTCUTS_ENABLED = false;
-if (__SHORTCUTS_ENABLED__) {
-  // minifier will drop this condition if __SHORTCUTS_ENABLED__ = false
-  SHORTCUTS_ENABLED = true;
-}
 const paths = { base: './', assets: 'assets/' };
 const gameConfig = getConfigBase();
 
